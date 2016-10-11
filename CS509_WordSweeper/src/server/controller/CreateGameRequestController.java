a2d89834270515f8df6f176a3410a8fe29685287
Valar <valar_3726@163.com>
2016-10-11 19:54:20 -0400

    upload first

---
+++ CS509_WordSweeper/src/server/controller/CreateGameRequestController.java
@@ -0,0 +1,75 @@
+package server.controller;
+
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+import server.ClientState;
+import server.IProtocolHandler;
+import server.model.ServerModel;
+import xml.Message;
+
+import java.util.Random;
+
+/**
+ * Controller on server to package up the current state of the model
+ * as an updateResponse message and send it back to the client.
+ */
+public class CreateGameRequestController implements IProtocolHandler {
+
+	ServerModel model;
+	
+	public CreateGameRequestController (ServerModel model) {
+		this.model = model;
+	}
+	
+	public Message process(ClientState client, Message request) {
+		
+		model.joinGame();  // HACK.
+		
+		// note you can retrieve information from the request...
+		Node createRequest = request.contents.getFirstChild();
+		NamedNodeMap map = createRequest.getAttributes();
+		
+		String pname = map.getNamedItem("name").getNodeValue();
+		Random random=new java.util.Random();
+		//String [][] strArray = new String [7][7];
+		String temp =new String("");
+		String content =new String("");
+		for(int i=0;i<7;i++)
+		{
+			for(int j=0;j<7;j++)
+			{
+				temp = String.valueOf((char)(random.nextInt(26)+65));
+				model.setstrArray(i, j, temp);
+			}
+			
+		}
+		
+		for(int i=0;i<7;i++)
+		{
+			for(int j=0;j<7;j++)
+			{
+				content = content + model.getstrArray(i, j);
+			}
+		}
+		int x = random.nextInt(3);
+		int y = random.nextInt(3);
+		String player_content =new String("");
+		for(int i= y;i<y+4;i++)
+		{
+			for(int j=x;j<x+4;j++)
+			{
+				player_content += model.getstrArray(i, j);
+			}
+		}
+		// Construct message reflecting state
+		String xmlString = Message.responseHeader(request.id()) +
+				"<boardResponse gameId='hg12jhd' managingUser='" + pname + "' bonus='7,7' contents='" + content + "'>" +
+			      "<player name='" + pname + "' score='0' position='"+ x +","+ y +"' board='"+ player_content +"'/>" +
+			  "</boardResponse>" +
+			"</response>";
+		
+		// send this response back to the client which sent us the request.
+		return new Message (xmlString);
+	}
+}
---
+++ CS509_WordSweeper/src/server/controller/JoinGameRequestController.java
@@ -0,0 +1,71 @@
+package server.controller;
+
+import java.util.Random;
+
+import server.ClientState;
+import server.IProtocolHandler;
+import server.Server;
+import server.model.ServerModel;
+import xml.Message;
+
+/**
+ * Controller on server to package up the current state of the model
+ * as an updateResponse message and send it back to the client.
+ */
+public class JoinGameRequestController implements IProtocolHandler {
+
+	ServerModel model;
+	
+	public JoinGameRequestController (ServerModel model) {
+		this.model = model;
+	}
+	
+	public Message process(ClientState client, Message request) {
+		
+		model.joinGame(); 
+		Random random=new java.util.Random();
+		String otherPlayers = "";
+		String content =new String("");
+		for(int i=0;i<7;i++)
+		{
+			for(int j=0;j<7;j++)
+			{
+				content = content + model.getstrArray(i, j);
+			}
+		}
+		for (int i = 0; i < model.getNumPlayers(); i++) {
+			int x = random.nextInt(3);
+			int y = random.nextInt(3);
+			String player_content =new String("");
+			for(int m= y;m<y+4;m++)
+			{
+				for(int n=x;n<x+4;n++)
+				{
+					player_content += model.getstrArray(m, n);
+				}
+			}
+			otherPlayers += "<player name='player" + i + "' score='0' position='"+ x +","+ y +"' board='"+ player_content +"'/>";
+		}
+		
+		// Construct message reflecting state
+		String xmlString = Message.responseHeader(request.id()) +
+				"<boardResponse gameId='hg12jhd' managingUser='player0' bonus='4,3' contents='"+ content +"'>" +
+			      otherPlayers +
+			  "</boardResponse>" +
+			"</response>";
+		
+		Message message = new Message (xmlString);
+		
+		// all other players on game (excepting this particular client) need to be told of this
+		// same response. Note this is inefficient and should be replaced by more elegant functioning
+		// hint: rely on your game to store player names...
+		for (String id : Server.ids()) {
+			if (!id.equals(client.id())) {
+				Server.getState(id).sendMessage(message);
+			}
+		}
+
+		// send this response back to the client which sent us the request.
+		return message;
+	}
+}
