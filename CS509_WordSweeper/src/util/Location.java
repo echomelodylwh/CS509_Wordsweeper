b89b18c6b1626b75ac587cbc753d0e5a4636b340
TaraZhu <TaraZhu@users.noreply.github.com>
2016-11-08 19:45:13 -0500

    Add files via upload

---
+++ CS509_WordSweeper/src/util/Location.java
@@ -0,0 +1,38 @@
+package server.util;
+
+public class Location {
+	int column;
+	int row;
+	
+	
+	public Location(int column, int row) {
+		super();
+		this.column = column;
+		this.row = row;
+	}
+
+
+	public int getColumn() {
+		return column;
+	}
+
+
+	public int getRow() {
+		return row;
+	}
+
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	
+	
+	
+
+}
